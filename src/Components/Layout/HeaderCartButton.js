6ac62d8cc9eb1aae287c4e1a7887df5668b07bbd
tanmayJain <114196274+mad-tan@users.noreply.github.com>
2023-01-19 21:57:00 -0600

    cart button animations added

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,17 +1,38 @@
 import classes from "./HeaderCartButton.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../store/cart-context";
 
-import { AiOutlineShoppingCart  } from "react-icons/ai";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const HeaderCartButton = (props) => {
-const cartCtx = useContext(CartContext);
+  const [btnUpdated, setBtnUpdated] = useState(false)
 
-const cartNumber = cartCtx.items.reduce((currNumber,item)=>{return currNumber + item.amount},0)
+  const cartCtx = useContext(CartContext);
+
+  const {items} = cartCtx;
+
+  const cartNumber = items.reduce((currNumber, item) => {
+    return currNumber + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${btnUpdated ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if(items.length===0)  return;
+    setBtnUpdated(true)
+    
+    const timer = setTimeout(()=>{
+      setBtnUpdated(false)
+    },300)
+
+    return ()=>{
+      clearTimeout(timer)
+    }
+  }, [items]);
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
-      <span  className={classes.icon}>{AiOutlineShoppingCart}</span>
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>{AiOutlineShoppingCart}</span>
       <span>Your Cart</span>
       <span className={classes.badge}>{cartNumber}</span>
     </button>
